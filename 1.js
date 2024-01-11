e28792e1773157771cd8ac3e2972bafb6ac597b9
xdonaskelva1 <127114805+xdonaskelva1@users.noreply.github.com>
2024-01-11 23:31:57 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,22 +1,9 @@
-window.onload = function() {
-    var images = document.querySelectorAll('.custom-post-content img');
-    images.forEach(function(img) {
-        img.onload = function() {
-            if (this.naturalHeight > this.naturalWidth) {
-                img.classList.add("vertical-image");
-            } else {
-                img.classList.add("horizontal-image");
-            }
-        };
-    });
-
-    // Clear floats after every three vertical images
-    var verticalImages = document.querySelectorAll('.custom-post-content img.vertical-image');
-    verticalImages.forEach(function(img, index) {
-        if ((index + 1) % 3 === 0) {
-            var clearDiv = document.createElement('div');
-            clearDiv.style.clear = 'both';
-            img.parentNode.insertBefore(clearDiv, img.nextSibling);
+document.addEventListener("DOMContentLoaded", function() {
+    var images = document.querySelectorAll('.mybbImage');
+    images.forEach(function(image) {
+        var aspectRatio = image.naturalWidth / image.naturalHeight;
+        if (aspectRatio < 1) { // Assuming vertical image if width is less than height
+            image.classList.add('verticalImage');
         }
     });
-};
+});
